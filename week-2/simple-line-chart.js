9ad40c633fc40eb3110befb7665eec4d85d6e705
Anders Opstrup <anders@coffeecode.dk>
2018-02-12 22:07:07 +0100

    Finished simple line chart

--- week-2/simple-line-chart.js
+++ week-2/simple-line-chart.js
@@ -4,7 +4,7 @@ var margin = {top: 30, right: 20, bottom: 30, left: 50},
     height = 270 - margin.top - margin.bottom;
 
 // Parse the date / time
-var parseDate = d3.timeFormat("%d-%b-%y").parse;
+var parseTime = d3.timeParse("%d-%b-%y");
 
 // Set the ranges
 var x = d3.scaleTime().range([0, width]);
@@ -26,9 +26,31 @@ var svg = d3.select(".simple-line-chart")
               .attr("height", height + margin.top + margin.bottom)
             .append("g")
               .attr("transform",
-                    "transform(" + margin.left + "," + margin.top + ")");
+                    "translate(" + margin.left + "," + margin.top + ")");
 
 // Read csv file
 d3.csv("data.csv", function(err, data){
+    if (err) {
+        throw error;
+    }
 
-})
+    data.forEach(function(d) {
+        d.date = parseTime(d.date);
+        d.close = +d.close;
+    });
+
+    x.domain(d3.extent(data, function(d) { return d.date; }));
+    y.domain([0, d3.max(data, function(d) { return d.close; })]);
+
+    svg.append("path")
+        .data([data])
+        .attr("class", "line")
+        .attr("d", valueline);
+
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+    svg.append("g")
+        .call(d3.axisLeft(y));
+});
