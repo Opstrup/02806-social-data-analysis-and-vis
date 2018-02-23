45dfeab11f6aa57e1aee3aec0b41887190b421fa
Anders Opstrup <anders@coffeecode.dk>
2018-02-23 10:40:33 +0100

    Added a pie chart to the document

--- week-4/week-4-script.js
+++ week-4/week-4-script.js
@@ -1,54 +1,44 @@
-// Scales
-// Domain is input for scales and range is what range the output should be in
-// between.
-var dataset = [
-  [5, 20], [480, 90], [250, 50], [100, 33], [330, 95],
-  [410, 12], [475, 44], [25, 67], [85, 21], [220, 88]
- ];
+var dataset = [5, 10, 20, 45, 6 , 25];
+var pie = d3.pie();
+var color = d3.scaleOrdinal(d3.schemeCategory10);
+var pieDataset = pie(dataset);
 
-var myScale = d3.scaleLinear()
-                .domain([100, 500])
-                .range([10, 350])
+var h = 300;
+var w = 300;
 
-console.log('Input 100 to scaler', myScale(100))
-console.log('Input 300 to scaler', myScale(300))
-console.log('Input 500 to scaler', myScale(500))
+var outerRadius = w / 2;
+var innerRadius = 0;
+var arc = d3.arc()
+            .innerRadius(innerRadius)
+            .outerRadius(outerRadius);
 
-// loops through the dataset and returns the max value of the dataset (x axis)
-d3.max(dataset, function(d){ 
-  return d[0]; 
-})
+var svg = d3.select(".pie-chart")
+            .append("svg")
+            .attr("width", w)
+            .attr("height", h);
 
-d3.min(dataset, function(d){
-  return d[0];
-})
+// Setting up groups for each arc
+var arcs = svg.selectAll("g.arc")
+              .data(pieDataset)
+              .enter()
+              .append("g")
+              .attr("class", "arc")
+              .attr("transform", "translate(" + outerRadius + ", " + outerRadius + ")");
 
-// The first element in each array in the dataset is the x value and the second
-// is the y value
+// Drawing each arc path
+arcs.append("path")
+    .attr("fill", function(d, i) {
+      return color(i);
+    })
+    .attr("d", arc);
 
-// Padding pushes all the elements inside the SVG so they don't get cut at the 
-// edges
-var padding = 20;
-
-// Setting up x-axis scale
-var xScale = d3.scaleLinear()
-               .domain([0, d3.max(dataset, function(d){ return d[0]; })])
-               .range([padding, 350 - padding])
-
-// Setting up y-axis scale
-// Reversing the range for y-axis because the svg element has reversed values
-// high y-values is low in the svg (not like a regular coordinate sys)
-var yScale = d3.scaleLinear()
-               .domain([0, d3.max(dataset, function(d){ return d[1]; })])
-               .range([350 - padding, padding])
-
-
-// Axis
-// Axis can be Top, Left, Bottom and Right
-var xAxis = d3.axisBottom(xScale).ticks(5);
-var yAxis = d3.axisLeft(yScale).ticks(5);
-
-// Use formatter to convert data to different formats
-var myFormat = d3.format(".1%");
-console.log('0.54321 formatted with myFormat is:', myFormat(0.54321));
-
+// Add labels
+arcs.append("text")
+    .attr("transform", function(d) {
+      return "translate(" + arc.centroid(d) + ")";
+    })
+    .attr("text-anchor", "middle")
+    .attr("fill", "white")
+    .text(function(d) {
+      return d.value;
+    });
