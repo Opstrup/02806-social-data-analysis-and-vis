bf9296f51f7a8029dbee674e43e6af7003130925
Anders Opstrup <anders@coffeecode.dk>
2018-03-07 08:50:21 +0100

    Added scripts for the charts

---
+++ week-5/networkChart.js
@@ -0,0 +1,1 @@
+var dataset = { nodes: [ { name: "Manhattan" }, { name: "Brooklyn" }, { name: "Queens" }, { name: "The Bronx" }, { name: "Staten Island" } ], edges: [ { source: 0, target: 1 }, { source: 0, target: 2 }, { source: 0, target: 3 }, { source: 0, target: 4 }, { source: 1, target: 3 }, { source: 2, target: 3 }, { source: 2, target: 4 }, { source: 3, target: 4 }, ] };
